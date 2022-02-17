40f6cd43b7c2856b14ec31b4e94c90471cca6080
Brian L. Troutwine <brian@troutwine.us>
2022-02-17 09:06:48 -0800

    [RETYPIST] applied 3 modifications

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -4,7 +4,7 @@ mod server;
 mod tap;
 
 pub(self) use schema::build_schema;
-pub use server::Server;
+pub(super) use server::Server;
 use tokio::sync::oneshot;
 
 // Shutdown channel types used by the server and tap.
