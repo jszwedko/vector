04e1c4745d607533a87db44a90ba3d45776ef2b7
Brian L. Troutwine <brian@troutwine.us>
2022-02-17 21:43:01 -0800

    [RETYPIST] applied 1 modifications

--- src/internal_events/elasticsearch.rs
+++ src/internal_events/elasticsearch.rs
@@ -4,7 +4,7 @@ use vector_core::internal_event::InternalEvent;
 #[derive(Debug)]
 pub struct ElasticSearchEventEncoded {
     pub(crate) byte_size: usize,
-    pub index: String,
+    pub(crate) index: String,
 }
 
 impl InternalEvent for ElasticSearchEventEncoded {
